2583c4766155a196eb8c62ed3dbefa6056885c6c
Elif ztürk <nozturk@sabanciuniv.edu>
2022-11-17 21:21:54 +0300

    worked on product page's backend and frontend services

--- src/Components/productBody.js
+++ src/Components/productBody.js
@@ -4,10 +4,16 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import List from "./list";
 
-export default function ProductBody() {
+export default function ProductBody(props) {
+  const image = props.image || "";
+  const player = props.player || "";
+  const owner = props.owner || "";
+  const type = props.type || "";
+  const sold = props.sold || "";
+  console.log(owner, type, player, sold);
   return (
     <Container maxWidth="xl" sx={{ height: 600 }}>
-      <List></List>
+      <List player={player} owner={owner} type={type} sold={sold}></List>
     </Container>
   );
 }
