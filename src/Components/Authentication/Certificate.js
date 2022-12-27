b465ea025448336b1e9c9f0dab3a87e6bdcbd17f
Rafi Banana <97386315+rafibanana@users.noreply.github.com>
2022-12-28 00:59:39 +0300

    working on certificate

--- src/Components/Authentication/Certificate.js
+++ src/Components/Authentication/Certificate.js
@@ -14,14 +14,36 @@ export default function Certificate(props) {
         props.close();
     };
     return (
-        <Box sx={{ bgcolor: "primary", borderRadius: 2, maxWidth: 600, position: "fixed", top: "30%", left: "40%", zIndex: 1 }}>
+        <Box sx={{ flexDirection: "column", bgcolor: "primary", borderRadius: 2, maxWidth: 600, position: "fixed", top: "30%", left: "35%", zIndex: 1 }}>
             <div className="show">
-                <CardMedia
-                    component="img"
-                    src={info.image}
-                    style={{ width: 400, height: 500 }}
-                />
-                <Box sx={{ bgcolor: "primary", alignItems: "center", position: "absolute", left: "39%", top: "30%", zIndex: 1, height: 300, width: 400 }}>
+                <Box sx={{ justifyContent: "center", alignItems: "center", mb: 3 }}>
+                    <Typography variant="h30" sx={{ fontWeight: 400, fontSize: "15px", color: "red" }}>
+                        CONGRATULATIONS, You successfully authenticated your shirt.
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{ display: "flex", flexDirection: "row", bgcolor: "background.paper", borderRadius: 3, justifyContent: "center", alignItems: "center" }}>
+                    <CardMedia
+                        style={{ width: 300, height: 300 }}
+                        src={info.image}
+                        component="img"
+                    />
+                    <Box sx={{ display: "flex", flexDirection: "column" }}>
+                        <Typography variant="h30" sx={{ fontWeight: 400, fontSize: "17px", color: "green" }}>
+                            Name: {info.name}
+                        </Typography>
+                        <Typography variant="h30" sx={{ fontWeight: 400, fontSize: "17px", color: "blue" }}>
+                            Type: {info.type}
+                        </Typography>
+                        <Typography variant="h30" sx={{ fontWeight: 400, fontSize: "17x", color: "pink" }}>
+                            Owner: {info.owner}
+                        </Typography>
+                        <Typography variant="h30" sx={{ fontWeight: 400, fontSize: "17px", color: "red" }}>
+                            Price: {info.price}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box sx={{ bgcolor: "primary", alignItems: "center", position: "absolute", bottom: "25%", right: "10%", zIndex: 1, height: 300, width: 400 }}>
                     <IconButton
                         aria-label="close"
                         onClick={handleCancel}
